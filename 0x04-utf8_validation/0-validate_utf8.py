07546a3ce4576818152af2f3824b869a16637da5
onoyima <clintonfaze@outlook.com>
2024-08-23 19:52:55 -0700

    correction

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,38 +1,40 @@
 #!/usr/bin/python3
-"""
-UTF-8 Validation Module
-"""
+""" UTF-8 Validation """
 
 
 def validUTF8(data):
     """
-    Method that determines if a given data set represents
-    a valid UTF-8 encoding.
-    Args:
-        data (List[int]): A list of integers where each integer
-        represents 1 byte of data.
-    Returns:
-        bool: True if data is a valid UTF-8 encoding, else False.
+    Method that determines if a given data set represents a valid
+    UTF-8 encoding.
     """
-    n_bytes = 0
+    number_bytes = 0
 
-    for num in data:
-        bin_rep = format(num, '#010b')[-8:]
+    mask_1 = 1 << 7
+    mask_2 = 1 << 6
 
-        if n_bytes == 0:
-            if bin_rep[0] == '0':
+    for i in data:
+
+        mask_byte = 1 << 7
+
+        if number_bytes == 0:
+
+            while mask_byte & i:
+                number_bytes += 1
+                mask_byte = mask_byte >> 1
+
+            if number_bytes == 0:
                 continue
-            elif bin_rep[:3] == '110':
-                n_bytes = 1
-            elif bin_rep[:4] == '1110':
-                n_bytes = 2
-            elif bin_rep[:5] == '11110':
-                n_bytes = 3
-            else:
-                return False
-        else:
-            if bin_rep[:2] != '10':
-                return False
-        n_bytes -= 1
 
-    return n_bytes == 0
+            if number_bytes == 1 or number_bytes > 4:
+                return False
+
+        else:
+            if not (i & mask_1 and not (i & mask_2)):
+                    return False
+
+        number_bytes -= 1
+
+    if number_bytes == 0:
+        return True
+
+    return False
