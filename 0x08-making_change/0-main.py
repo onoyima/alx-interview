1a6247258a7096db95faf8a57b814b7c257bab86
onoyima <clintonfaze@outlook.com>
2024-09-18 06:35:58 -0700

    0-making_change.py

--- 0x08-making_change/0-main.py
+++ 0x08-making_change/0-main.py
@@ -5,6 +5,8 @@ Main file for testing the makeChange function
 
 makeChange = __import__('0-making_change').makeChange
 
-print(makeChange([1, 2, 25], 37))  # Expected output: 7 (25+10+2)
-print(makeChange([1256, 54, 48, 16, 102], 1453))  # Expected output: -1 (Impossible)
+print(makeChange([1, 2, 25], 37))
+# Expected output: 7 (25+10+2)
+print(makeChange([1256, 54, 48, 16, 102], 1453))
+# Expected output: -1 (Impossible)
 
--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -3,13 +3,13 @@
 Module 0-making_change
 """
 
+
 def makeChange(coins, total):
     """
     Determine the fewest number of coins needed to meet a given amount total
     Arguments:
     coins -- list of the values of the coins in possession
     total -- total amount to achieve with the fewest number of coins
-    
     Return:
     The fewest number of coins needed to meet the total
     If total is 0 or less, return 0
@@ -18,7 +18,8 @@ def makeChange(coins, total):
     if total <= 0:
         return 0
 
-    # Initialize a list to store the minimum coins required for each amount up to 'total'
+    # Initialize a list to store the minimum coins
+    # required for each amount up to 'total'
     dp = [float('inf')] * (total + 1)
     dp[0] = 0  # Zero coins are needed to make a total of 0
 
