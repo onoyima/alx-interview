c52c6570a349e852a4129ce2bf8051bf74f8560b
onoyima <clintonfaze@outlook.com>
2024-09-19 12:23:11 -0700

    ghgj

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -6,10 +6,12 @@ Module 0-making_change
 
 def makeChange(coins, total):
     """
-    Determine the fewest number of coins needed to meet a given amount total
+    Determine the fewest number of coins needed to
+    meet a given amount total
     Arguments:
     coins -- list of the values of the coins in possession
-    total -- total amount to achieve with the fewest number of coins
+    total -- total amount to achieve with the
+    fewest number of coins
     Return:
     The fewest number of coins needed to meet the total
     If total is 0 or less, return 0
@@ -17,15 +19,16 @@ def makeChange(coins, total):
     """
     if total <= 0:
         return 0
-
-    # Initialize a list to store the minimum coins
-    # required for each amount up to 'total'
     dp = [float('inf')] * (total + 1)
-    dp[0] = 0  # Zero coins are needed to make a total of 0
+    dp[0] = 0
+    coins.sort(reverse=True)
 
-    # Dynamic programming to fill the dp list
     for coin in coins:
         for amount in range(coin, total + 1):
-            dp[amount] = min(dp[amount], dp[amount - coin] + 1)
+            if dp[amount - coin] != float('inf'):
+                dp[amount] = min(dp[amount], dp[amount - coin] + 1)
+
+        if dp[total] != float('inf'):
+            return dp[total]
 
     return dp[total] if dp[total] != float('inf') else -1
