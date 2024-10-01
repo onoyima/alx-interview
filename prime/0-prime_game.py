2c56c650e75ef907ea48a0512d05300cfabe5337
onoyima <clintonfaze@outlook.com>
2024-10-01 05:39:28 +0100

    this

---
+++ prime/0-prime_game.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""0. Prime Game - clinton and jude are playing a game"""
+
+
+def isWinner(x, nums):
+    """x - rounds
+    nums - numbers list
+    """
+    if x <= 0 or nums is None:
+        return None
+    if x != len(nums):
+        return None
+
+    jude = 0
+    clinton = 0
+
+    a = [1 for x in range(sorted(nums)[-1] + 1)]
+    a[0], a[1] = 0, 0
+    for i in range(2, len(a)):
+        rm_multiples(a, i)
+
+    for i in nums:
+        if sum(a[0:i + 1]) % 2 == 0:
+            jude += 1
+        else:
+            clinton += 1
+    if jude > clinton:
+        return "jude"
+    if clinton > jude:
+        return "clinton"
+    return None
+
+
+def rm_multiples(ls, x):
+    """removes multiple
+    of primes
+    """
+    for i in range(2, len(ls)):
+        try:
+            ls[i * x] = 0
+        except (ValueError, IndexError):
+            break
