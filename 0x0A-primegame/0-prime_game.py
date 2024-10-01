db7f5ae8a03c5f2be8f55e2722a50b47657b2724
onoyima <clintonfaze@outlook.com>
2024-10-01 07:20:53 -0700

    0x0A-primegame

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,42 +1,56 @@
 #!/usr/bin/python3
-"""0. Prime Game - maria and ben are playing a game"""
 
+def sieve_of_eratosthenes(n):
+    """ Generate a list of primes up to n using the Sieve of Eratosthenes """
+    is_prime = [True] * (n + 1)
+    is_prime[0], is_prime[1] = False, False  # 0 and 1 are not primes
+    for i in range(2, int(n ** 0.5) + 1):
+        if is_prime[i]:
+            for j in range(i * i, n + 1, i):
+                is_prime[j] = False
+    return is_prime
+
+def count_prime_moves(n, primes):
+    """ Count how many moves are possible given a set of numbers up to n """
+    taken = [False] * (n + 1)
+    moves = 0
+    
+    for num in range(2, n + 1):
+        if primes[num] and not taken[num]:  # If it's a prime and not taken
+            moves += 1
+            # Remove the prime and its multiples
+            for multiple in range(num, n + 1, num):
+                taken[multiple] = True
+    return moves
 
 def isWinner(x, nums):
-    """x - rounds
-    nums - numbers list
-    """
-    if x <= 0 or nums is None:
-        return None
-    if x != len(nums):
+    """ Determine the winner after x rounds of the game """
+    if x == 0 or not nums:
         return None
 
-    ben = 0
-    maria = 0
+    max_n = max(nums)  # Get the largest number in nums
+    primes = sieve_of_eratosthenes(max_n)
+    
+    maria_wins = 0
+    ben_wins = 0
 
-    a = [1 for x in range(sorted(nums)[-1] + 1)]
-    a[0], a[1] = 0, 0
-    for i in range(2, len(a)):
-        rm_multiples(a, i)
-
-    for i in nums:
-        if sum(a[0:i + 1]) % 2 == 0:
-            ben += 1
+    # Play x rounds
+    for n in nums:
+        moves = count_prime_moves(n, primes)
+        # If the number of moves is odd, Maria wins; otherwise, Ben wins
+        if moves % 2 == 0:
+            ben_wins += 1
         else:
-            maria += 1
-    if ben > maria:
-        return "ben"
-    if maria > ben:
-        return "maria"
-    return None
+            maria_wins += 1
 
+    # Determine the overall winner
+    if maria_wins > ben_wins:
+        return "Maria"
+    elif ben_wins > maria_wins:
+        return "Ben"
+    else:
+        return None
 
-def rm_multiples(ls, x):
-    """removes multiple
-    of primes
-    """
-    for i in range(2, len(ls)):
-        try:
-            ls[i * x] = 0
-        except (ValueError, IndexError):
-            break
+# Example usage:
+if __name__ == "__main__":
+    print("Winner: {}".format(isWinner(5, [2, 5, 1, 4, 3])))
