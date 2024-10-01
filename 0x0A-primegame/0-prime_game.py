f30b0e19be6afc4f9975523c3b511ae0846a96b6
onoyima <clintonfaze@outlook.com>
2024-10-01 07:45:06 -0700

    0x0A-primegame

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,56 +1,57 @@
 #!/usr/bin/python3
 
-def sieve_of_eratosthenes(n):
-    """ Generate a list of primes up to n using the Sieve of Eratosthenes """
-    is_prime = [True] * (n + 1)
-    is_prime[0], is_prime[1] = False, False  # 0 and 1 are not primes
-    for i in range(2, int(n ** 0.5) + 1):
-        if is_prime[i]:
-            for j in range(i * i, n + 1, i):
-                is_prime[j] = False
-    return is_prime
 
-def count_prime_moves(n, primes):
-    """ Count how many moves are possible given a set of numbers up to n """
-    taken = [False] * (n + 1)
-    moves = 0
-    
-    for num in range(2, n + 1):
-        if primes[num] and not taken[num]:  # If it's a prime and not taken
-            moves += 1
-            # Remove the prime and its multiples
-            for multiple in range(num, n + 1, num):
-                taken[multiple] = True
-    return moves
+def sieve_of_eratosthenes(n):
+    """Generate a list of primes up to n using the Sieve of Eratosthenes"""
+    sieve = [True] * (n + 1)
+    sieve[0] = sieve[1] = False  # 0 and 1 are not primes
+    for i in range(2, int(n ** 0.5) + 1):
+        if sieve[i]:
+            for j in range(i * i, n + 1, i):
+                sieve[j] = False
+    return [i for i in range(2, n + 1) if sieve[i]]
+
 
 def isWinner(x, nums):
-    """ Determine the winner after x rounds of the game """
-    if x == 0 or not nums:
+    """
+    Determine the winner of each game.
+    
+    :param x: Number of rounds.
+    :param nums: List of 'n' values for each round.
+    :return: Name of the player that won the most rounds, or None if it's a tie.
+    """
+    if not nums or x < 1:
         return None
 
-    max_n = max(nums)  # Get the largest number in nums
-    primes = sieve_of_eratosthenes(max_n)
-    
-    maria_wins = 0
-    ben_wins = 0
+    max_n = max(nums)
+    primes_up_to_max_n = sieve_of_eratosthenes(max_n)
 
-    # Play x rounds
+    # Memoization for each n up to max_n
+    prime_count_up_to = [0] * (max_n + 1)
+
+    # Count the number of primes up to each number n
+    for i in range(2, max_n + 1):
+        prime_count_up_to[i] = prime_count_up_to[i - 1] + (1 if i in primes_up_to_max_n else 0)
+
+   maria = 0
+    ben = 0
+
     for n in nums:
-        moves = count_prime_moves(n, primes)
-        # If the number of moves is odd, Maria wins; otherwise, Ben wins
-        if moves % 2 == 0:
-            ben_wins += 1
+        if prime_count_up_to[n] % 2 == 1:
+            # If the number of primes is odd, Maria wins (she goes first)
+           maria += 1
         else:
-            maria_wins += 1
+            # If the number of primes is even, Ben wins
+            ben += 1
 
-    # Determine the overall winner
-    if maria_wins > ben_wins:
+    ifmaria > ben:
         return "Maria"
-    elif ben_wins > maria_wins:
+    elif ben >maria:
         return "Ben"
     else:
         return None
 
-# Example usage:
+
+# Sample usage
 if __name__ == "__main__":
     print("Winner: {}".format(isWinner(5, [2, 5, 1, 4, 3])))
